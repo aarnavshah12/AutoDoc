1bb2c1efc52b48299aa7c7ffb7c3bb6b890b21d7
Lucas Oliva~ <lucas.l.oliva@gmail.com>
2024-06-08 13:20:21 -0400

    Comment on multiple files in current dir

--- extension.js
+++ extension.js
@@ -40,12 +40,16 @@ function activate(context) {
             vscode.window.showInformationMessage('No active editor found!');
         }
     })
-    const CommentCodeBase = vscode.commands.registerCommand("autodoc.CommentCodeBase",async function(){
+    const CommentCodeBase = vscode.commands.registerCommand("autodoc.CommentCurrentDir",async function(){
         const editor = vscode.window.activeTextEditor;
         if(editor) {
-            const filePath = editor.document.fileName;
+            let filePath = editor.document.fileName
             try {
-                await fileProcessing.getFiles("/home/lloliva/Coding/Projects/JamHacks8/AutoDoc/libraries");
+                const files = await fileProcessing.getFiles(filePath);
+                const len = files.length
+                for(let i = 0; i<len;i++){
+                    await fileProcessing.processFile(files[i]);
+                }
                 vscode.window.showInformationMessage('File has been Documented successfully!');
             } catch (error) {
                 vscode.window.showErrorMessage('Error Documenting the file: ' + error.message);
